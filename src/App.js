468d7d0fae5aaaf375c303d183666fe8e14b4c1a
Adam Harvey <heyitsmeharv@gmail.com>
2022-12-24 12:06:18 +0000

    .

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import Snowfall from 'react-snowfall'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Snowfall />
+        Merry Christmas, Marie!
+        <br />
+        <br />
+        You now own www.vosiejewellery.com
+      </header>
+    </div>
+  );
+}
+
+export default App;
