02b09b3e0e13cc05ee28db1b2a83ecd61dc8cdf2
Adam Harvey <heyitsmeharv@gmail.com>
2022-12-24 12:24:55 +0000

    added logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import logo from './resources/images/vosie.jpg';
 import './App.css';
+
 
 import Snowfall from 'react-snowfall'
 
 function App() {
   return (
     <div className="App">
+      <img alt="vosie logo" src={logo} />
       <header className="App-header">
         <Snowfall />
         Merry Christmas, Marie!
